dce3b433140c75050aaade97806924bdcc27cafc
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-01-06 01:19:49 +0530

    update it

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ config();
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({
+    message: 'Hello World!'
+  });
 })
 app.listen(process.env.PORT,()=>{
   console.log(`Server is running on port ${process.env.PORT}`);
